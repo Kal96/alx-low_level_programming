fb54ff67b27f33e9a4322edb4f7cebcaefaa0f89
Kal96 <kalimicynthia96@gmail.com>
2023-07-17 04:31:26 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a < 10; a++)
+	{
+		putchar(a);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
